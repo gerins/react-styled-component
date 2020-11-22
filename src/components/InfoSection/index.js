4a5c3c1e0310420d01211dc36eb51f9ef8e14e84
Garin Prakoso <gerin.rin@gmail.com>
2020-11-22 20:17:55 +0700

    first app on reactJS

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  Column2,
+  ImgWrap,
+  Img,
+} from "./InfoElements";
+import { Button } from "../ButtonElement";
+
+function InfoSection({ id }) {
+  return (
+    <>
+      <InfoContainer id={id}>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>Top Line</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to='home'>Button</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+}
+
+export default InfoSection;
